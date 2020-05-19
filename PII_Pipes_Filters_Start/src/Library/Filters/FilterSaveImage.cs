35531230a1e79af50d4b94d3b72bdf8721e410b2
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-05-19 17:30:17 -0300

    elimino contador

--- PII_Pipes_Filters_Start/src/Library/Filters/FilterSaveImage.cs
+++ PII_Pipes_Filters_Start/src/Library/Filters/FilterSaveImage.cs
@@ -9,12 +9,12 @@ namespace CompAndDel.Filters
 {
     public class FilterSaveImage : IFilter
     {
-        private int counter = 1;
+        
         public IPicture Filter(IPicture image)
         {
             IPicture savePicture = image.Clone();
             PictureProvider p = new PictureProvider();
-            p.SavePicture(savePicture, $"NewImage{counter++}.jpg");
+            p.SavePicture(savePicture, "NewImage.jpg");
             return savePicture;
         }
         
